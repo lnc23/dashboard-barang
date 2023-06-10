c1c828f45163607979972371e957b2b0b8bfd98d
yansenfer <yansenferdinand6@gmail.com>
2023-06-10 20:38:23 +0700

    toast page

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -76,7 +76,7 @@ export const SideBar = ({ children }: Props) => {
         },
         {
           name: "Toast",
-          path: "/",
+          path: "/toastpage",
           icon: <RxComponent1 />,
         },
         {
